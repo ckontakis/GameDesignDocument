67afa795de43e77e15aa11af15a057cdcfb944c2
ckontakis <ckontakis@csd.auth.gr>
2021-04-02 12:37:25 +0300

    2/4/2021

---
+++ Javascript/WorldBuilding.js
@@ -0,0 +1,36 @@
+/*
+Function to show and hide an element.
+ */
+function showFunction(el){
+    let x = document.getElementById(el);
+    if (x.style.visibility === "visible"){
+        x.style.visibility = "hidden";
+    }else{
+        x.style.visibility = "visible";
+    }
+}
+
+/*
+Function to show and hide elements. The first parameter is the element that we want to show and hide and the second parameter
+is the font of the button that we change.
+ */
+function showAndHide(el, btnFont){
+    let x = document.getElementById(el);
+    let y = document.getElementById(btnFont);
+    if(x.style.display === 'none'){
+        x.style.display = 'block';
+        y.className = 'fa fa-minus';
+    }else{
+        x.style.display = 'none';
+        y.className = 'fa fa-plus';
+    }
+}
+
+function showPassword() {
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
