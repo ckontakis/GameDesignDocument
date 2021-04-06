2fb79079abeb59987293858865b7f41a8ac849d2
ckontakis <ckontakis@csd.auth.gr>
2021-04-06 21:01:30 +0300

    6/4/2021

--- JavaScript/Main.js
+++ JavaScript/Main.js
@@ -1,9 +1,22 @@
 function confirmDelete(){
     let con = confirm('Are you sure you want to delete this game design document?');
 
-    if(con === true){
+    if(con){
         return true;
     }else{
         return false;
+    }
+}
+
+function showCategories(el, fontArrow){
+    let x = document.getElementById(el);
+    let y = document.getElementById(fontArrow);
+
+    if(x.style.display === 'block'){
+        x.style.display = 'none';
+        y.className = 'fa fa-chevron-down'
+    }else{
+        x.style.display = 'block';
+        y.className = 'fa fa-chevron-up'
     }
 }
